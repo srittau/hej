ebf85942766acbe97deff6cf6d2b84826574b3b5
Sebastian Rittau <srittau@rittau.biz>
2022-01-09 23:09:37 +0100

    Fix test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,17 @@
-import React from "react";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { render, screen } from "@testing-library/react";
+import React from "react";
+
 import App from "./App";
 
+const client = new ApolloClient({ cache: new InMemoryCache() });
+
 test("renders add note button", () => {
-  render(<App />);
+  render(
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>,
+  );
   const linkElement = screen.getByRole("button", { name: "Add note" });
   expect(linkElement).toBeInTheDocument();
 });
