c22ce23ea17c2de1eea308b368cf98b65cb0f295
Sebastian Rittau <srittau@rittau.biz>
2022-01-10 00:11:57 +0100

    Add static files to uvicorn app

--- pylibs/hej/app.py
+++ pylibs/hej/app.py
@@ -1,7 +1,20 @@
+import os
+from pathlib import Path
+
 from starlette.applications import Starlette
+from starlette.staticfiles import StaticFiles
 
 from .debug import debug
 from .gql import app as gql_app
 
+
+def static_path() -> Path:
+    path = os.getenv("HEJ_STATIC_PATH")
+    if path is not None:
+        return Path(path)
+    return Path("build")
+
+
 app = Starlette(debug=debug())
 app.mount("/graphql", gql_app)
+app.mount("/", StaticFiles(directory=static_path(), html=True), name="static")
