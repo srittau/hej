d227205cd1b03e73ff8f825e579c51ebc147c484
Sebastian Rittau <srittau@rittau.biz>
2022-01-10 16:24:01 +0100

    Send logout to API when logging out

--- src/Logout.tsx
+++ src/Logout.tsx
@@ -1,9 +1,17 @@
 import { unsetAuthCookie } from "./auth";
+import { useLogout } from "./gql";
 
 export default function Logout() {
+  const sendLogout = useLogout();
+
+  function logout() {
+    sendLogout();
+    unsetAuthCookie();
+  }
+
   return (
     <div className="logout-button">
-      <button type="button" onClick={() => unsetAuthCookie()}>
+      <button type="button" onClick={logout}>
         Logout
       </button>
     </div>
