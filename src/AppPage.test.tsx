b879609cfaa2449ae49ba1a5fed4aa4ad1d54363
Sebastian Rittau <srittau@rittau.biz>
2022-01-10 16:02:41 +0100

    Fix test

--- src/AppPage.test.tsx
+++ src/AppPage.test.tsx
@@ -2,14 +2,14 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { render, screen } from "@testing-library/react";
 import React from "react";
 
-import App from "./App";
+import AppPage from "./AppPage";
 
 const client = new ApolloClient({ cache: new InMemoryCache() });
 
 test("renders add note button", () => {
   render(
     <ApolloProvider client={client}>
-      <App />
+      <AppPage />
     </ApolloProvider>,
   );
   const linkElement = screen.getByRole("button", { name: "Add note" });
