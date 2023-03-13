b2800c30556e3ec4cce2bce187669987cc79c349
Sebastian Rittau <srittau@rittau.biz>
2023-03-13 21:09:31 +0100

    Switch to vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -8,5 +9,10 @@ export default defineConfig({
     proxy: {
       "/graphql": "http://127.0.0.1:8080/graphql",
     },
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./src/setupTests.ts"],
   },
 });
