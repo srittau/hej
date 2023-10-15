dbe16ca332db690172ebf185a687d68ba0b28fa7
Sebastian Rittau <srittau@rittau.biz>
2023-10-15 16:13:43 +0200

    Extend `DatabaseFixture`
    * Add `select_all_rows()`.
    * `select_only_row()` now fails if multiple rows exist.
    * Add `assert_rows_equal()` and `assert_one_row_equals()`.

--- pylibs/hej/testutil_db.py
+++ pylibs/hej/testutil_db.py
@@ -1,5 +1,5 @@
 import datetime
-from collections.abc import Mapping
+from collections.abc import Mapping, Sequence
 from pathlib import Path
 from sqlite3 import Row
 from typing import Any, AsyncGenerator, LiteralString
@@ -55,21 +55,66 @@ class DatabaseFixture:
         )
         return uuid
 
+    async def select_all_rows(self, table_name: LiteralString) -> list[Row]:
+        rows_iter = self.db.execute_fetchall(f"SELECT * FROM {table_name}")
+        return [row async for row in rows_iter]
+
     async def select_only_row(self, table_name: LiteralString) -> Row:
-        row = await self.db.execute_fetchone(f"SELECT * FROM {table_name}")
-        if row is None:
+        rows = await self.select_all_rows(table_name)
+        if len(rows) == 0:
             raise AssertionError(f"No rows in table {table_name}")
-        return row
+        elif len(rows) > 1:
+            raise AssertionError(f"Multiple rows in table {table_name}")
+        else:
+            return rows[0]
 
     async def assert_only_row_equals(
-        self, table_name: LiteralString, values: Mapping[str, Any]
+        self, table_name: LiteralString, expected_row: Mapping[str, Any]
     ) -> None:
         row = await self.select_only_row(table_name)
-        for k, v in values.items():
+        for k, v in expected_row.items():
             dv = _sql_value(v)
             assert (
                 row[k] == dv
             ), f"Row {k} does not match: {dv!r} != {row[k]!r}"
+
+    async def assert_rows_equal(
+        self,
+        table_name: LiteralString,
+        expected_rows: Sequence[Mapping[str, Any]],
+    ) -> None:
+        rows = [*await self.select_all_rows(table_name)]
+        assert len(rows) == len(
+            expected_rows
+        ), f"Row count does not match: {len(expected_rows)} != {len(rows)}"
+        for expected_row in expected_rows:
+            for row in rows:
+                if row_matches(row, expected_row):
+                    rows.remove(row)
+                    break
+            else:
+                raise AssertionError(
+                    f"Row {expected_row} not found in {table_name}"
+                )
+
+    async def assert_one_row_equals(
+        self,
+        table_name: LiteralString,
+        expected_row: Mapping[str, Any],
+    ) -> None:
+        rows = [*await self.select_all_rows(table_name)]
+        for row in rows:
+            if row_matches(row, expected_row):
+                return
+        raise AssertionError(f"Row {expected_row} not found in {table_name}")
+
+
+def row_matches(row: Row, expected_row: Mapping[str, Any]) -> bool:
+    for k, v in expected_row.items():
+        dv = _sql_value(v)
+        if row[k] != dv:
+            return False
+    return True
 
 
 def _sql_value(v: object) -> Any:
