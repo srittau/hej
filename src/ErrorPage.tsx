be4d5bb31073724dd33408dce2aec8abdbd10acb
Sebastian Rittau <srittau@rittau.biz>
2024-03-12 18:41:42 +0100

    Show actual error message on error page

--- src/ErrorPage.tsx
+++ src/ErrorPage.tsx
@@ -1,17 +1,27 @@
 import { Link, isRouteErrorResponse, useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
-  const error = useRouteError();
+  const [title, text] = useError();
   return (
     <div className="error-page">
-      <h1>
-        {isRouteErrorResponse(error)
-          ? `${error.status} ${error.statusText}`
-          : "Unknown error"}
-      </h1>
+      <h1>{title}</h1>
       <div>
+        <p>{text}</p>
         <Link to="/">Back to main page</Link>
       </div>
     </div>
   );
+}
+
+function useError(): [string, string] {
+  const error = useRouteError();
+  let title = "Unknown error";
+  let text = String(error);
+  if (isRouteErrorResponse(error)) {
+    title = `${error.status} ${error.statusText}`;
+  } else if (error instanceof Error) {
+    title = error.name;
+    text = error.message;
+  }
+  return [title, text];
 }
