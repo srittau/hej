5daf888c977eed4305caa775aeeb3d7d41c757ed
Sebastian Rittau <srittau@rittau.biz>
2024-05-21 21:04:36 +0200

    Move logos in to images sub-directory

--- src/Header.tsx
+++ src/Header.tsx
@@ -12,8 +12,8 @@ export default function Header({ opened, toggle }: HeaderProps) {
   return (
     <Group p="xs" className={classes.header}>
       <Burger opened={opened} hiddenFrom="sm" onClick={toggle} />
-      <Image src="/hej-logo.svg" className={classes.logo} />
-      <Image src="/hej-name.svg" alt="Hej" className={classes.name} />
+      <Image src="/images/hej-logo.svg" className={classes.logo} />
+      <Image src="/images/hej-name.svg" alt="Hej" className={classes.name} />
       <Logout ml="auto" />
     </Group>
   );
