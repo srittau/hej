0ff5068a1012af032a653c18351f206173cbd139
Sebastian Rittau <srittau@rittau.biz>
2024-05-21 21:30:19 +0200

    Fix opening the app on a sub-route
    For example, opening /notes would return a 404 error.

--- pylibs/hej/app.py
+++ pylibs/hej/app.py
@@ -1,10 +1,13 @@
 import logging
 import os
 import sys
+from collections.abc import Callable
 from pathlib import Path
 
 from starlette.applications import Starlette
-from starlette.routing import Mount
+from starlette.requests import Request
+from starlette.responses import FileResponse
+from starlette.routing import Mount, Route
 from starlette.staticfiles import StaticFiles
 
 from .db import migrate_db
@@ -20,15 +23,31 @@ def static_path() -> Path:
     return Path("dist")
 
 
+def file_app(
+    path: str,
+) -> Callable[[Request], FileResponse]:
+    def file_app(request: Request) -> FileResponse:
+        return FileResponse(static_path() / path)
+
+    return file_app
+
+
 def create_app() -> Starlette:
     app = Starlette(
         routes=[
             Mount("/graphql", gql_app()),
             Mount(
-                "/",
-                StaticFiles(directory=static_path(), html=True),
-                name="static",
+                "/assets",
+                StaticFiles(directory=static_path() / "assets"),
+                name="assets",
             ),
+            Mount(
+                "/images",
+                StaticFiles(directory=static_path() / "images"),
+            ),
+            Route("/manifest.json", file_app("manifest.json")),
+            Route("/robots.txt", file_app("robots.txt")),
+            Route("/{p:path}", file_app("index.html")),
         ],
         debug=debug(),
     )
