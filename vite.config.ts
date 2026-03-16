a8c1ba81789de53cfd9764de02b70bc068ec457c
Sebastian Rittau <sebastian.rittau@zfutura.de>
2026-03-16 11:54:09 +0100

    Update vite to 8.0.0
    Also update @vitejs/plugin-react to 6.0.1 and vitest to 4.1.0

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 /// <reference types="vitest" />
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
+  resolve: {
+    tsconfigPaths: true,
+  },
   server: {
     proxy: {
       "/graphql": "http://127.0.0.1:8080/graphql",
